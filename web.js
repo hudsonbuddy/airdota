0856ed2934c1fbc3b3758f109867184f6a8b0752
hudson <archreth@gmail.com>
2014-08-01 16:22:18 -0700

    working locally

--- web.js
+++ web.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
