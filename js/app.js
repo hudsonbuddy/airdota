983f7cc66a8381dd0a5e601a9fed141586da6828
hudson <archreth@gmail.com>
2014-09-30 16:57:29 -0700

    finished essays and dict and article as well

--- js/app.js
+++ js/app.js
@@ -2,22 +2,27 @@ var app = angular.module('airdota', ['ngSanitize', 'ngRoute']);
 //This is the main method of the app
 
 app.run(function($rootScope) {
-      $rootScope.name = "Hudson";
+
 });
 
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+    
+    $locationProvider.html5Mode(true);
 
+    //Navigation Routes
     $routeProvider.otherwise({redirectTo: '/'});
     $routeProvider.when('/', {templateUrl:'/templates/home.html'});
+    $routeProvider.when('/essays', {templateUrl:'/templates/essays.html'});
+    $routeProvider.when('/dict', {templateUrl:'/templates/dict.html'});
+    $routeProvider.when('/entry', {templateUrl:'/templates/dictentry.html'});
 
-    $locationProvider.html5Mode(true);
+    //Essay Routes
+    $routeProvider.when('/beginnings', {templateUrl:'/essays/beginnings.html'});
+
     
-
 
 }]);
 
 app.controller('airdotaController', function($scope) {
     
-    $scope.person = "Duan"; 
-
 });
